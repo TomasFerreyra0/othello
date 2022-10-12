a599c8c2fd7314ec43c9b800cc3b9394c17a4510
Tomas <tomas.benjamin.f@gmail.com>
2022-10-11 21:36:29 -0300

    Borrando prueba

--- othello Game.py
+++ othello Game.py
@@ -1,4 +1,2 @@
 #-----------------------------------------------TRABAJO PRACTICO 1 PROGRAMACION-----------------------------------------------
 #Integrantes del grupo: Franco Pierabella y Tomas Benjamin Ferreyra
-
-#esto es una prueba
